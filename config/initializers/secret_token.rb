f2cc3d2b2a9b55f9fff59c51e2fd83940f48ef5e
Lorgio Antonio Jimenez <lorgio.a.jimenez@aexp.com>
2011-05-20 11:01:37 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsQuiz::Application.config.secret_token = '8dd21dae54c558bb732c9a4984bfa498f0eee80954818955da958f862a91377d11b86b99a8015388db646f39dc1d24f9a3e09bd741119838f7002fcca142b353'
